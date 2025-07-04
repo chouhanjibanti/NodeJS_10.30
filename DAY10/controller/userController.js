4d031b56e844c6e70a661d05c87de1791cf851be
Banti Chouhan <chouhanbanti224@gmail.com>
2025-07-04 12:03:43 +0530

    day10

--- DAY10/controller/userController.js
+++ DAY10/controller/userController.js
@@ -1,10 +1,9 @@
 // const User= require('../model/userModel');
 
-// //  password hashing 
+// //  password hashing
 // const bcryptjs = require('bcryptjs');
 
-
-// // logic for hashing 
+// // logic for hashing
 // const securePassword = async (password)=>{
 //     try {
 //         const passwordHash = await bcryptjs.hash(password ,10)
@@ -27,8 +26,8 @@
 //          image :req.file.filename,
 //          type : req.body.type,
 //     })
-  
-//     // check email is exist  or not 
+
+//     // check email is exist  or not
 //       const userData = User.findOne({email :req.body.email})
 //       if(userData){
 //         res.status(200).send({success : false , message :"user already exist"});
@@ -36,7 +35,6 @@
 //          const user_data= await user.save();
 //          res.status(200).send({success:true , data:user_data})
 //       }
-
 
 //     } catch (error) {
 //          res.status(400).send(error.message) // check on the api
@@ -47,54 +45,110 @@
 //     registerUser,
 // }
 
-
 // =======================================
 
-
-const User = require('../model/userModel');
-const bcryptjs = require('bcryptjs');
+const User = require("../model/userModel");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
 
 // Function to hash password
 const securePassword = async (password) => {
-    try {
-        const passwordHash = await bcryptjs.hash(password, 10);
-        return passwordHash;
-    } catch (error) {
-        console.log("error", error);
-    }
+  try {
+    const passwordHash = await bcryptjs.hash(password, 10);
+    return passwordHash;
+  } catch (error) {
+    console.log("error", error);
+  }
 };
 
 const registerUser = async (req, res) => {
-    try {
-        const { name, email, password, mobile, type } = req.body;
+  try {
+    const { name, email, password, mobile, type } = req.body;
 
-        // Check if user already exists
-        const userData = await User.findOne({ email: email });
-        if (userData) {
-            return res.status(200).send({ success: false, message: "User already exists" });
-        }
-
-        // Hash the password
-        const spassword = await securePassword(password);
-
-        // Create new user
-        const user = new User({
-            name,
-            email,
-            password: spassword,
-            mobile,
-            image: req.file.filename,
-            type,
-        });
-
-        const savedUser = await user.save();
-        res.status(200).send({ success: true, data: savedUser });
-
-    } catch (error) {
-        res.status(400).send({ success: false, message: error.message });
+    // Check if user already exists
+    const userData = await User.findOne({ email: email });
+    if (userData) {
+      return res
+        .status(200)
+        .send({ success: false, message: "User already exists" });
     }
+
+    // Hash the password
+    const spassword = await securePassword(password);
+
+    // Create new user
+    const user = new User({
+      name,
+      email,
+      password: spassword,
+      mobile,
+      image: req.file.filename,
+      type,
+    });
+
+    const savedUser = await user.save();
+    res.status(200).send({ success: true, data: savedUser });
+  } catch (error) {
+    res.status(400).send({ success: false, message: error.message });
+  }
+};
+
+const create_token = async (id) => {
+  try {
+    const token = await jwt.sign({ _id: id }, config.secret_jwt);
+    return token;
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+// login method
+
+const user_login = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const userData = await User.findOne({ email: email });
+    if (userData) {
+      const passwordMatch = await bcryptjs.compare(password, userData.password);
+      if (passwordMatch) {
+        const tokenData = await create_token(userData.id);
+
+        const useResult = {
+          _id: userData._id,
+          name: userData.name,
+          email: userData.email,
+          password: userData.password,
+          image: userData.image,
+          mobile: userData.mobile,
+          type: userData.type,
+          token: tokenData,
+        };
+
+        const response = {
+          success: true,
+          msg: "user details",
+          data: useResult,
+        };
+        res.status(200).send(response);
+      } else {
+        res
+          .status(200)
+          .send({ success: false, msg: "login details are incorrect.." });
+      }
+    } else {
+      res
+        .status(200)
+        .send({ success: false, msg: "login details are incorrect.." });
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
 module.exports = {
-    registerUser,
+  registerUser,
+  user_login
 };
--- DAY10/routes/userRoute.js
+++ DAY10/routes/userRoute.js
@@ -1,50 +1,44 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const user_controller = require('../controller/userController');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const user_controller = require("../controller/userController");
 
 const user_route = express.Router();
 
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
 user_route.use(bodyParser.json());
-user_route.use(bodyParser.urlencoded({extended:true}))
+user_route.use(bodyParser.urlencoded({ extended: true }));
 
-
-user_route.use(express.static("public"))
+user_route.use(express.static("public"));
 
 const storage = multer.diskStorage({
-    destination : function(req,file,cb){
-        cb(
-            null ,
-            path.join(__dirname , "../public/userImages"),
-            function(err,success){
-                if(err) throw err;
-            }
-        )
-    },
-    filename: function(req,file,cb){
-        const name =  Date.now() + "_" + file.originalname;
-        cb(null , name , function(error, success1){
-            if(error) throw error
-        })
+  destination: function (req, file, cb) {
+    cb(
+      null,
+      path.join(__dirname, "../public/userImages"),
+      function (err, success) {
+        if (err) throw err;
+      }
+    );
+  },
+  filename: function (req, file, cb) {
+    const name = Date.now() + "_" + file.originalname;
+    cb(null, name, function (error, success1) {
+      if (error) throw error;
+    });
+  },
+});
 
-    }
-})
+const upload = multer({ storage: storage });
 
+user_route.post(
+  "/register",
+  upload.single("image"),
+  user_controller.registerUser
+);
 
-const upload =multer({storage : storage})
-
-
-user_route.post("/register", upload.single("image"),user_controller.registerUser)
+// for login
+user_route.post("/login", user_controller.user_login);
 
 module.exports = user_route;
-
-
-
-
-
-
-
-
-
